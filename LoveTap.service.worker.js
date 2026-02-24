50e11b55285b99945f38320f409a4937a28849a8
SemasPing <semasping@gmail.com>
2026-02-24 03:13:07 +0100

    0.0.4

--- LoveTap.service.worker.js
+++ LoveTap.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771898389|15947730';
+const CACHE_VERSION = '1771899150|777030817';
 /** @type {string} */
 const CACHE_PREFIX = 'Love Tap Idle: C-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
